7ec660f21ec8e4ac6b9d304728e611c498155a99
khalidabbood <khalidwaleedabbood@gmail.com>
2025-03-20 16:07:44 +0100

    Prototype

---
+++ assignment/py-in-gh.py
@@ -0,0 +1,20 @@
+from typing import cast, Any
+import Rhino.Geometry as rg #type: ignore
+import networkx as nx #type: ignore
+
+# DECLARE INPUT VARIABLES
+x = cast(rg.Circle, x)  # type: ignore
+y = cast(int, y)  # type: ignore
+
+
+# YOUR CODE HERE
+x.Radius = y # type: ignore
+
+
+
+# OUTPUT`
+a =x
+
+#added a comment
+#another comment
+#circle
